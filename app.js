5ad9b1173e0db4c8a2ec447f1751c1f6dda13231
Dwyer <38626923+D-wyz@users.noreply.github.com>
2018-10-15 02:22:22 -0400

    User login functionality

--- app.js
+++ app.js
@@ -3,6 +3,15 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var mongoose = require('mongoose');
+var methodOverride = require('method-override');
+require('dotenv').config();
+var cors = require('cors');
+const passport = require('passport');
+
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true })
+  .then(() => console.log('MONGODB CONNECTED'))
+  .catch((err) => console.log(err))
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -13,11 +22,16 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hjs');
 
+app.use(passport.initialize());
+require('./utils/passport')(passport);
+
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(methodOverride('_method'));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
+var authMiddleware = require('../utils/authMiddleware');
+var passport = require('passport');
 
-/* GET users listing. */
+/* GET home page. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.render('index', { title: 'Express' });
 });
 
+router.post('/createuserorlogin', authMiddleware.checkSignUp, authMiddleware.checkSignIn);
+
+router.get('/current-user', passport.authenticate('jwt', {session: false}), authMiddleware.successUser);
 module.exports = router;
