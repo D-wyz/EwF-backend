31f07672fbd17f5ae010be954ec5c530eeda38ea
Dwyer <michael.todd.dwyer@gmail.com>
2018-10-25 11:43:34 -0400

    two-page login / register

--- routes/users.js
+++ routes/users.js
@@ -5,11 +5,60 @@ var passport = require('passport');
 var authMiddleware = require('../utils/authMiddleware');
 var userController =require('../controllers/userController')
 
+const validateRegisterInput = require('../validation/register');
+const validateLoginInput = require('../validation/login');
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.post('/register', function(req, res, next) {
+
+
+  const { errors, isValid } = validateRegisterInput(req.body);
+  console.log(errors)
+  // Check Validation
+  if (!isValid) {
+    return res.status(400).json(errors);
+    //return res.status(400).json(errors);
+  }
+
+  userController.register(req.body)
+    .then(user => res.json(user))
+    .catch(err => res.json(err));
+
+});
+
+router.post('/login', function(req, res) {
+  const { errors, isValid } = validateLoginInput(req.body);
+  // Check Validation
+  if (!isValid) {
+    return res.status(400).json(errors);
+  }
+
+  userController.login(req.body)
+    .then(user => res.json(user))
+    .catch(err => res.json(err));
+
+});
+
+router.get('/current',
+  passport.authenticate('jwt', { session: false }),
+  (req, res) => {
+    res.json({
+      id: req.user.id,
+      name: req.user.name,
+      email: req.user.email
+    });
+  }
+);
+
+
+module.exports = router;
+
+
+// SPA version
 router.post('/createuserorlogin', authMiddleware.checkSignUp, authMiddleware.checkSignIn);
 
 router.get('/getuser', function (req, res, next) {
--- utils/game.js
+++ utils/game.js
@@ -2,6 +2,14 @@ module.exports = {
     generateChallenge() {
         // game logic for generating challenges goes here
         return { name: 'Test', postion: [2, 1] }
+    },
+    scoreUser(user) {
+        console.log(user);
+        
+    },
+    teamUser(team) {
+        console.log(team);
+        
     }
 }
 
